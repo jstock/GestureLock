388c1dcbc6633a5ba36212a1da94d967ec2f21b0
jiemingyi <jiemingyi@gmail.com>
2014-10-28 17:18:03 -0400

    Added a welcome text and two buttons: "Create a Gesture" and "View Saved Gestures." Set the "Create a Gesture" button to start the SaveGesture.java activity.

--- app/src/main/java/com/example/gesturelock/gesturelock/GestureScreen.java
+++ app/src/main/java/com/example/gesturelock/gesturelock/GestureScreen.java
@@ -1,18 +1,34 @@
 package com.example.gesturelock.gesturelock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class GestureScreen extends Activity {
+
+    private Button mGotoCreateGestureButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture_screen);
+
+
+        mGotoCreateGestureButton =  (Button) findViewById(R.id.createGesture_button);
+
+        mGotoCreateGestureButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GestureScreen.this, SaveGesture.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
