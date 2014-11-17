bb72775b75c19be35454587fc5190c001381ce41
Brett Koenig <koenig.337@osu.edu>
2014-11-17 15:35:54 -0500

    Added the ability to create a user, and log in with created username/password, using internal storage. Didn't commit app/app.iml and .idea/misc.xml

---
+++ app/src/main/java/com/example/gesturelock/gesturelock/CreateUser.java
@@ -0,0 +1,103 @@
+package com.example.gesturelock.gesturelock;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+/**
+ * Created by Brett on 11/17/2014.
+ */
+public class CreateUser extends Activity {
+
+    private EditText userLog;
+    private EditText pass;
+    private String data;
+    private String file = "mydata";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_create_screen);
+        userLog = (EditText)(findViewById(R.id.username));
+        pass = (EditText)(findViewById(R.id.createPassword));
+        Button btnClose = (Button) findViewById(R.id.button1);
+        Intent i = getIntent();
+        // Binding Click event to Button
+        btnClose.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                data = "\n:Username:" + userLog.getText().toString() + ":password:" + pass.getText().toString() + ":\n";
+                boolean flag = false;
+                try {
+                    FileInputStream fin = openFileInput(file);
+                    int c;
+                    String temp="";
+                    while( (c = fin.read()) != -1){
+                        temp = temp + Character.toString((char)c);
+                    }
+                    if(pass.getText().toString().isEmpty() || userLog.getText().toString().isEmpty())
+                    {
+                        flag = true;
+                        Toast.makeText(getBaseContext(), "You must enter a username and a password.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    if(temp.indexOf(userLog.getText().toString()) > -1)
+                    {
+                        flag = true;
+                        Toast.makeText(getBaseContext(), "A user with that name already exists, please try again.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    if(!flag) {
+                        //needs to be Context.MODE_APPEND
+                        FileOutputStream fOut = openFileOutput(file, Context.MODE_APPEND);
+                        fOut.write(data.getBytes());
+                        fOut.close();
+                        Toast.makeText(getBaseContext(), "User created",
+                                Toast.LENGTH_SHORT).show();
+                        //Closing SecondScreen Activity
+                        finish();
+                    }
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    //Closing SecondScreen Activity
+                    finish();
+                }
+
+            }
+        });
+
+    }
+    public void read(View view){
+        try{
+            FileInputStream fin = openFileInput(file);
+            int c;
+            String temp="";
+            while( (c = fin.read()) != -1){
+                temp = temp + Character.toString((char)c);
+            }
+            userLog.setText(temp);
+            Toast.makeText(getBaseContext(), "file read",
+                    Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e){
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.gesture_screen, menu);
+        return true;
+    }
+
+}
--- app/src/main/java/com/example/gesturelock/gesturelock/LoginScreen.java
+++ app/src/main/java/com/example/gesturelock/gesturelock/LoginScreen.java
@@ -11,12 +11,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.sql.Time;
 
 
 /**
  * Created by Zak on 10/31/2014.
+ * Edited by Brett on 11/17/2014
  */
 public class LoginScreen extends Activity {
 
@@ -26,6 +29,8 @@ public class LoginScreen extends Activity {
     private TextView loginAttempts;
     private Button login;
     int counter = 3;
+    //BK below
+    private String file = "mydata";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +41,77 @@ public class LoginScreen extends Activity {
         loginAttempts = (TextView)findViewById(R.id.textViewAttemptCount);
         loginAttempts.setText(Integer.toString(counter));
         login = (Button)findViewById(R.id.buttonLogin);
+        Button btnNextScreen = (Button) findViewById(R.id.btnNextScreen);
+
+        //Listening to button event
+        btnNextScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), CreateUser.class);
+
+                //Sending data to another Activity
+                //nextScreen.putExtra("name", inputName.getText().toString());
+                //nextScreen.putExtra("email", inputEmail.getText().toString());
+
+                //Log.e("n", inputName.getText() + "." + inputEmail.getText());
+
+                startActivity(nextScreen);
+
+            }
+        });
     }
 
     public void login(View view) {
 
-        if (username.getText().toString().toUpperCase().equals("ADMIN"))
+        boolean loginFlag = false;
+        try{
+            FileInputStream fin = openFileInput(file);
+            int c;
+            String temp="";
+            String[] tokens;
+            int counter = 0;
+            String delims ="[:]";
+            while( (c = fin.read()) != -1){
+                temp = temp + Character.toString((char)c);
+            }
+            //see if username is in localStorage
+            if(temp.indexOf(username.getText().toString()) == -1 && !username.getText().toString().toUpperCase().equals("ADMIN"))
+            {
+              Toast.makeText(getBaseContext(), "Sorry there is no such user in our database.",
+                      Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                //see if the username matches the password if it does set loginFlag to true, if not leave loginflag false
+                tokens = temp.split(delims);
+                boolean found = false;
+                String testString;
+                while(counter < tokens.length && !found)
+                {
+                    testString = tokens[counter].toString().trim();
+                    if(testString.equals(username.getText().toString())) {
+                        found = true;
+                        counter++;
+                    }
+                    counter++;
+                }
+                if(tokens[counter].equals(password.getText().toString()))
+                {
+                    loginFlag = true;
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "Incorrect password.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }catch(Exception e){
+
+        }
+
+        if (username.getText().toString().toUpperCase().equals("ADMIN") || loginFlag)
         {
             startActivity(new Intent(getApplicationContext(), GestureScreen.class));
         }
